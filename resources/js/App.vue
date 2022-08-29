8966024b21809c3469c7deca2207447c47aafe07
NickAstley <andreanicastro1994@gmail.com>
2022-08-30 00:42:23 +0200

    Created simple api route to display all posts on the homepage

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>Ciaone</h1>
+    <div>
+        <Navbar></Navbar>
+        <div class="container">
+            <PostList></PostList>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Navbar from "./components/Navbar.vue";
+    import PostList from "./components/PostList.vue";
     export default {
-
+        components: { Navbar, PostList }
     }
 </script>
---
+++ resources/js/components/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="row row-cols-3">
+            <div class="col" v-for="post in posts" :key="post.id">
+                <div class="card">
+                    <img :src="'/storage/' + post.cover_img" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        <p class="card-text">{{ post.content }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                posts: []
+            }
+        },
+
+        methods:  {
+            fetchPosts() {
+                Axios.get("/api/posts")
+                    .then((resp) => {
+                        this.posts = resp.data
+                })
+            }
+        },
+
+        mounted() {
+            this.fetchPosts();
+        }
+    }
+</script>
