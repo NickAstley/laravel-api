6aa6a2085033efb7f2f8e55484c10b17c3344609
NickAstley <andreanicastro1994@gmail.com>
2022-08-30 02:31:47 +0200

    Added placeholder for posts with no cover

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,6 +10,12 @@ class PostController extends Controller
 {
     public function index() {
         $posts = Post::all();
+        $posts = $posts->map(function ($post) {
+            if($post->cover_img) {
+                $post->cover_img = asset("/storage/" . $post->cover_img);
+            }
+            return $post;
+        });
         return response()->json($posts);
     }
 }
--- resources/js/components/PostList.vue
+++ resources/js/components/PostList.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <div class="py-4">
         <div class="row row-cols-3">
             <div class="col" v-for="post in posts" :key="post.id">
                 <div class="card">
-                    <img :src="'/storage/' + post.cover_img" class="card-img-top" alt="...">
+                    <img :src="getCoverSrc(post)" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ post.title }}</h5>
                         <p class="card-text">{{ post.content }}</p>
@@ -31,6 +31,12 @@ import Axios from 'axios';
                     .then((resp) => {
                         this.posts = resp.data
                 })
+            },
+            getCoverSrc(post) {
+                if(!post.cover_img) {
+                    return "/img/placeholder.webp"
+                }
+                return post.cover_img
             }
         },
 
@@ -39,3 +45,10 @@ import Axios from 'axios';
         }
     }
 </script>
+
+<style lang="scss">
+    .card-img-top {
+        aspect-ratio: 21 / 9;
+        object-fit: cover;
+    }
+</style>
